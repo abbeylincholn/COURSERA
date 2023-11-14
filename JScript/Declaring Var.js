2ecc7f5212bef057b22ace6e8a8ebb1a68e063f4
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-11-14 23:23:57 +0000

    OR || operator

--- JScript/Declaring Var.js
+++ JScript/Declaring Var.js
@@ -14,3 +14,6 @@ console.log(petCat, "now says", catSound)
 var score = 8
 console.log("Mid-level skills:", score > 0 && score < 10)
 
+var timeRemaining = 0
+var energy = 10
+console.log("Game over: ", timeRemaining == 0 || energy == 0)
