b59d141516f23f933daba638d868ba1aaa4e89a3
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-11-17 04:44:02 +0000

    practice

--- JScript/Conditn_stmt.js
+++ JScript/Conditn_stmt.js
@@ -37,3 +37,18 @@ switch (place) {
     default:
         console.log('No medal');
 }
+
+// practice
+
+var age = 10
+
+if (age >= 65) {
+    console.log ('You get your income from your pension')
+} else if (age < 65 && age >= 18) {
+    console.log('Each month you get a salary')
+} else if (age < 18) {
+    console.log('You get an allowance')
+} else { 
+    console.log('The value of the age variable is not numerical')
+}
+
