66b0ea0e3f1240837b09a192d15f761e175bd235
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-11-18 03:41:03 +0000

    Loops

---
+++ Loop.js
@@ -0,0 +1,74 @@
+// Exercise// for loop. i++
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed')
+
+for (var i = 1; i <= 5; i++) {
+    console.log(i)
+} console.log('Counting completed')
+
+//------------------------------------
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+for (var i = 5; i > 0; i--) {
+    console.log(i)
+}; console.log('Counting Finished')
+
+
+// while loop. i = i - 1
+
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log('Counting completed!')
+
+var i = 1;
+while (i < 6) {
+    console.log(i);
+    i++
+};
+console.log('Counting completed');
+
+//--------------------------------------------
+
+console.log(5)
+console.log(4)
+console.log(3)
+console.log(2)
+console.log(1)
+console.log('Countdown finished!')
+
+var i = 5;
+while (i > 0 ) {
+    console.log(i);
+    i = i - 1
+};
+console.log('Counting finished')
+
+//----------------------------------------------
+
+console.log(2018)
+console.log(2019)
+console.log(2020)
+console.log(2021)
+console.log(2022)
+
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year++;
+};
+console.log('year finishing')
+
+
