9a6661cf7a5d7a1f61a822245a52627b19267353
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-11-18 04:39:00 +0000

    nested loop

--- Loop.js
+++ Loop.js
@@ -70,5 +70,33 @@ while (year < 2023) {
     year++;
 };
 console.log('year finishing')
+//-----------------------------------------------
 
+// nested loops - one inside another 
 
+for (var i = 2023; i < 2025; i++) {
+    console.log(i);
+    for (var j = 6; j < 9; j++) {
+        console.log("------", j);
+    }
+}
+//----------------------------------------------
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 10; secondNum++) {
+        console.log(firstNum + ", " + secondNum);
+    }
+}
+//---------------------------------------------
+
+for (var firstNum = 0; firstNum < 2; firstNum++) {
+    for (var secondNum = 0; secondNum < 10; secondNum++) {
+        console.log(firstNum + " times " + secondNum + " equals " + firstNum * secondNum);
+    }
+}
+//----------------------------------------------
+
+for (var i = 100; i > 10; i = i - 10) {
+    for (var j = 10; j > 4; j = j - 5) {
+        console.log(i + " divided by " + j + " equals " + i / j);
+    }
+}
