048e028fe32b15f551e399bb65184a5f2b276eb1
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-11-18 04:58:45 +0000

    nested loop css

--- Loop.js
+++ Loop.js
@@ -100,3 +100,17 @@ for (var i = 100; i > 10; i = i - 10) {
         console.log(i + " divided by " + j + " equals " + i / j);
     }
 }
+//---------------------------------------------------------------------
+var cashiers = ["cashier1", "cashier2", "cashier3", "cashier4", "cashier5", "cashier6"]
+
+for(var i = 0; i < cashiers.length; i += 1){
+  console.log(`Cashier at position ${i} is ${cashiers[i]}`)
+}
+//-------------------------------------------------------------
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < cubes.length; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
+//--------------------------------------------------------------------
