e3892dff53df0ab83b5efe5f6963ca2407b9cc5d
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-11-19 03:06:28 +0000

    arrays update

--- arreys.js
+++ arreys.js
@@ -6,10 +6,38 @@ function listArrayItems(arr) {
     }
 } var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 listArrayItems(colors); //display all items in the array at once
-//--------------------------------------------------------------------------
+
+//-------function that takes an array as input and display all items of this array
 function listArrayItems(arr) {
     for (var i = 0; i < arr.length; i++) {
-        console.log(i, arr[i]) //display the array item where the index is euqal to i
+        console.log(i, arr[i]) 
     }
 } var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
-listArrayItems(colors); //display all items in the array at once
+listArrayItems(colors); 
+
+//To start the count from one instead of zero, I can update my function declaration as follows: 
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i+1, arr[i])
+    }
+} var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
+
+//I can even add one or more conditions, such as:  
+function listArrayItems(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr [i] == 'red') {
+            console.log(i*100, "tomato")
+        } else {
+            console.log(i*100, arr[i])
+        }
+    }
+} var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
+//--------------------------------------------------------------
+function listArrayItems (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(i*50, arr[i])
+    }
+} var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+listArrayItems(colors);
