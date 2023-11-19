d71fbb9f6d8e245beb74c8af3ecc3c9918e39dca
Abiodun Sholanke <abbeylincholn4real@gmail.com>
2023-11-19 15:35:15 +0000

    update

--- JScript/js-traning-main/function2.js
+++ JScript/js-traning-main/function2.js
@@ -1,12 +1,21 @@
-function printSurfaceArea(x, y){
-    var width = x;
-    var height = y;
-    var area = width * height;
-    console.log("Surface area of: ("+x+" x "+y+") is: "+area);
+function printSurfaceArea(x, y) {
+  var width = x;
+  var height = y;
+  var area = width * height;
+  console.log("Surface area of: (" + x + " x " + y + ") is: " + area);
 }
-
 
 var width = 50;
 var height = 60;
 
-printSurfaceArea(width, height)
+printSurfaceArea(width, height);
+
+//-------------------------------------------------
+
+function printSurfaceArea(x, y) {
+  var width = x;
+  var height = y;
+  var area = width * height;
+  console.log("Surface area of: (" + x + " x " + y + ") is: " + area);
+}
+printSurfaceArea(10, 10);
